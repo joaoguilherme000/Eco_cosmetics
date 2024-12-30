2390d00a5ab134462b9807c6d8e256e01e80f881
joaoguilherme000 <joaoguilhermehig@gmail.com>
2024-12-29 23:48:32 -0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,8 @@
 'use strict';
 
-function changeImage(imageId = 0, imageId2 = 0) {
-  const im = document.getElementsByClassName('main__shop--product--box--img');
-  const im2 = document.getElementsByClassName('main__shop--product--box--img2');
+const form = document.getElementById('myform');
 
-  if (imageId !== 0) {
-    for (let i = 0; i < im.length; i++) {
-      im[i].style.display = 'none';
-    }
-
-    for (let i = 0; i < im2.length; i++) {
-      im2[i].style.display = 'none';
-    }
-
-    const selectedImage = document.getElementById(imageId);
-    const selectedImage2 = document.getElementById(imageId2);
-
-    selectedImage.style.display = 'block';
-    selectedImage2.style.display = 'block';
-  }
-}
-
-changeImage();
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  form.reset();
+});
